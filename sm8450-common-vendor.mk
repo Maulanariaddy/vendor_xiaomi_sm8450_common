f994561a4328a1701a052f95f7a554f44e3ecd25
chaitanya <chaitanyaarora3@gmail.com>
2024-09-22 17:58:20 +0530

    Revert "sm8450-common: Disable proprietary listen sound model"

--- sm8450-common-vendor.mk
+++ sm8450-common-vendor.mk
@@ -336,6 +336,8 @@ PRODUCT_PACKAGES += \
     libdsd2pcm \
     libdsi_netctrl \
     libdsutils \
+    libeai_fixed \
+    libeai_float \
     libeva \
     libeva_util \
     libface3d_dev \
@@ -356,6 +358,7 @@ PRODUCT_PACKAGES += \
     libkeymasterprovision \
     libkeymasterutils \
     liblbs_core \
+    liblistensoundmodel2vendor \
     libloc_api_v02 \
     libloc_api_wds \
     libloc_base_util \
@@ -610,6 +613,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.gnss-V5-ndk \
     vendor.qti.gnss-service \
     vendor.qti.hardware.AGMIPC@1.0-impl \
+    vendor.qti.hardware.ListenSoundModel@1.0-impl \
+    vendor.qti.hardware.ListenSoundModel@1.0 \
     vendor.qti.hardware.alarm@1.0 \
     vendor.qti.hardware.bluetooth_sar@1.0 \
     vendor.qti.hardware.bluetooth_sar@1.1 \
